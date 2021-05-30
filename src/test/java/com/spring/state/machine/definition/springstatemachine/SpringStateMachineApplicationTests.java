9a08c7733f32146040494f54014b48fe0f07ef98
satamguin <satam.guin@gmail.com>
2021-05-30 20:38:56 +0530

    Initial commit

---
+++ src/test/java/com/spring/state/machine/definition/springstatemachine/SpringStateMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.state.machine.definition.springstatemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStateMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
